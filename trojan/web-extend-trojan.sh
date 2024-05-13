5a8f083b264e3e0c9383d36936feb10532665242
Erwin <erwinproject.id@gmail.com>
2024-05-13 17:09:10 +0700

    Add files via upload

---
+++ trojan/web-extend-trojan.sh
@@ -0,0 +1,23 @@
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+clear
+user=$1
+masaaktif=$2
+exp=$(grep -wE "^#& $user" "/usr/local/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+now=$(date +%Y-%m-%d)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+exp3=$(($exp2 + $masaaktif))
+exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
+sed -i "/#& $user/c\#& $user $exp4" /usr/local/etc/xray/config.json
+systemctl restart xray
+FEEDBACK='{"id":"'${user}'","expired":"'${exp4}'","status": true,"msg":"Success"}'
+echo $FEEDBACK | python3 -mjson.tool
